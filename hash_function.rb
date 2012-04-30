b81eea8b802983c444dfd20e91309b1c8033f24b
andy <acslocum@thoughtworks.com>
2012-04-30 08:53:46 -0500

    generating random function for hash

--- hash_function.rb
+++ hash_function.rb
@@ -1,25 +1,32 @@
 class HashFunction
-  def initialize(val)
-    @val = val
-    generate_hash_array(val)
+  attr_reader :function
+  
+  def initialize(random_seed)
+    generate_hash_array(random_seed)
   end
   
   def hash(string_array)
-    total=0;
+    total=0
+    index=0
     string_array.each do |string|
       string.each_byte do |byte| 
-        total += @val*byte
+        total += scale(index)*byte
+        index += 1
       end
     end
     total
   end
   
+  private
+  
   def generate_hash_array(seed)
-    
+    srand seed
+    @function = []
+    128.times { @function << rand(256) }
   end
   
-  def function
-    []
+  def scale(index)
+    function[index%function.length]
   end
   
 end
